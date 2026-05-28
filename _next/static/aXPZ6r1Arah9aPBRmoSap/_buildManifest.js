a09d9bb54185e50c82e62871b78a1b99479f1c1a
YujieChen8888 <cqiao5060@gmail.com>
2026-05-28 21:14:54 +0800

    Deploy GitHub Pages

---
+++ _next/static/aXPZ6r1Arah9aPBRmoSap/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/counterfactual-load-forecasting-web//_next/:path+",
+        "destination": "/counterfactual-load-forecasting-web/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
